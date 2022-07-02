85f3d0d0bb67679a6866a5f06947dc596edd29ad
matthijsvanderplas <mvdplas84@ziggo.nl>
2022-07-02 19:19:09 +0200

    even-odd

---
+++ Front_end/JavaScript/even-odd-reporter.js
@@ -0,0 +1,17 @@
+for (let i = 0; i <= 20; i++) {
+    // console.log(`Is ${i} even or odd?`);
+    // TODO: determine if i is odd or even
+    // TODO: use console.log
+    if (i % 2 === 0) {
+        console.log(`${i} is an even`)
+    } else {
+        console.log(`${i} is an odd`)
+    }
+  }
+  
+  // Expected output:
+  // 1 is odd 
+  // 2 is even 
+  // 3 is odd 
+  // 4 is even 
+  // ...
