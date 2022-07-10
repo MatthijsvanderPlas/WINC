417a36419340580174b06e636e5a6cce63357543
matthijsvanderplas <mvdplas84@ziggo.nl>
2022-07-10 09:46:21 +0200

    Final Result (Combo of CSS and Javascript)

--- Front_end/JavaScript/ColorToggle/main.js
+++ Front_end/JavaScript/ColorToggle/main.js
@@ -12,22 +12,36 @@ function changeColor(e, color = 0) {
 	if(color === 0) {
 		document.body.style.backgroundColor = colors[e.target.value];
 		checkRadioButton(colors[e.target.value]);
-		
+		updateColorText(colors[e.target.value]);
+		closeColorBar();
 	} else {
 		document.body.style.backgroundColor = color;
-		checkRadioButton(color)
+		checkRadioButton(color);
+		updateColorText(color);
 	}
-}22
+}
 
 function checkRadioButton(color) {
 	arrayRadios.forEach((radio) => {
-		if(radio.value === color) {
-			radio.style.color = 'black';
-		}
+		if(radio.value === color) radio.checked = true;
 	})
 }
 
- arrayRadios.forEach((element) => {
+function updateColorText(color = 'White') {
+	const textSpan = document.querySelector('.card__span')
+	textSpan.textContent = ` ${color}`;
+}
+
+function closeColorBar() {
+	document.querySelector('.dropdown__hover').classList.remove('dropdown__hover');
+	setTimeout(() => {
+		document.querySelector('.navbar__dropdown').classList.add('dropdown__hover');
+	}, 2000);
+	
+}
+
+
+arrayRadios.forEach((element) => {
 	element.parentNode.style.backgroundColor = element.value;
 });
 
@@ -46,73 +60,3 @@ window.addEventListener('keydown', (e) =>{
 		}
 	}
 });
-
-
-
-
-// function closeColorbar() {
-// 	;
-// }
-
-// const addRadioEvent = function() {
-// 	const radios = document.querySelectorAll('input[name="colortoggle"]');
-// 	const lis = Array.prototype.slice.call(document.getElementsByClassName('colorbar__item'));
-
-// 	radios.forEach.call(radios, function(radio, i = 0) {
-	
-// 		lis[i].style.backgroundColor = radio.value;
-
-// 		radio.addEventListener('change', colorChange);
-		
-// 		i++;12
-// 	})
-// }
-
-// const updateCheckedRadio = function(numRadio) {
-// 	const radios = document.querySelectorAll('input[name="colortoggle"]');
-// 	radios.forEach((radio) => radio.setAttribute('checked', ''));
-// 	// radios[(numRadio-1)].setAttribute('checked', true)
-// }
-
-// const getKeystroke = function() {
-// 	const keyStrokes = {1: 'White', 2: 'Red',3: 'Yellow', 4: 'Blue',5: 'Green',6: 'Orange'};
-
-	
-// 		}
-// 	})
-// }
-
-// const colorbar = document.querySelector('.colorbar');
-
-// getKeystroke();
-// addRadioEvent();
-
-
-/* 
-Ideal situation
-
-we listen for keydown event to change the color
-we listen for hover event CSS
-User can change the color by clicking the button
-the Radio only indicates which one is active.
-
-hovering over the menu allready works as intended
-
-function that does a color change (either by btn click or keydown)
-function that sets the radio button
-
-
-
-
-*/
-
-// const colorChange = function(e, color = 0) {
-// 	if(color === 0) color = e.target.value;
-// 	document.body.style.backgroundColor = color;
-// 	document.querySelector('.card__span').textContent = ` ${color}`;
-// 	document.querySelector('.card__span').style.color = color;
-// 	if(color === 'White') {
-// 		document.querySelector('.card__span').style.color = 'black';
-// 	}
-// 	closeColorbar();
-// };
